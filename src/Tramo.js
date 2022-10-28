ce239ca2d26a01f7a0a2ef2e2d50f6910794b008
JaimeGM96 <jaimegallardo@correo.ugr.es>
2022-10-28 13:05:35 +0200

    Eliminado atributo consumo medio de Tramo (#11)

--- src/Tramo.js
+++ src/Tramo.js
@@ -1,7 +1,6 @@
 /**
  * Declaración de la clase Tramo. Esta clase representa un tramo de una ruta, que está compuesto por un punto de origen,
- * un destino, la distancia que hay entre esos dos puntos, el consumo medio de combustible que se 
- * gasta (dependerá del vehículo que recorra el tramo), el tiempo medio que se tarda en recorrer ese tramo
+ * un destino, la distancia que hay entre esos dos puntos, el tiempo medio que se tarda en recorrer ese tramo
  * y por último la hora de inicio que influirá en la toma de decisiones debido a que el tráfico puede variar en función de la hora.
  */
 class Tramo {
@@ -11,16 +10,14 @@ class Tramo {
      * @param {String} origen Origen del tramo
      * @param {String} destino Destino del tramo
      * @param {Number} distancia Distancia del tramo
-     * @param {Number} consumoMedio Consumo medio del tramo
      * @param {Number} tiempoMedio Tiempo medio del tramo
      * @param {Number} horaInicio Hora de inicio del tramo
      */
-    constructor (id, origen, destino, distancia, consumoMedio, tiempoMedio, horaInicio) {
+    constructor (id, origen, destino, distancia, tiempoMedio, horaInicio) {
         this.id = id;
         this.origen = origen;
         this.destino = destino;
         this.distancia = distancia;
-        this.consumoMedio = consumoMedio;
         this.tiempoMedio = tiempoMedio;
         this.horaInicio = horaInicio;
     }
@@ -55,14 +52,6 @@ class Tramo {
      */
     get distancia() {
         return this.distancia;
-    }
-
-    /**
-     * Método que devuelve el consumo medio del tramo
-     * @returns {Number} consumoMedio Consumo medio del tramo
-     */
-    get consumoMedio() {
-        return this.consumoMedio;
     }
 
     /**
