9f5d25eccd2ca3ae5e5efbbf2843440844bdeaf6
Pedro Martín González <pedromarting3@gmail.com>
2022-10-28 09:58:03 +0200

    avanza #14 modificando Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,57 +1,27 @@
 //Template tomado de https://gruntjs.com/sample-gruntfile
-//WIP
-module.exports = function(grunt) {
 
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      concat: {
-        options: {
-          separator: ';'
-        },
-        dist: {
-          src: ['src/**/*.js'],
-          dest: 'dist/<%= pkg.name %>.js'
-        }
+const { default: nodeTest } = require("node:test");
+
+//WIP
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    check: {
+      build: {
+        src: ["src/**/*.js"],
+        dest: "dist/<%= pkg.name %>.js",
       },
-      uglify: {
-        options: {
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-        },
-        dist: {
-          files: {
-            'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-          }
-        }
-      },
-      qunit: {
-        files: ['test/**/*.html']
-      },
-      jshint: {
-        files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-        options: {
-          // options here to override JSHint defaults
-          globals: {
-            jQuery: true,
-            console: true,
-            module: true,
-            document: true
-          }
-        }
-      },
-      watch: {
-        files: ['<%= jshint.files %>'],
-        tasks: ['jshint', 'qunit']
-      }
-    });
-  
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-  
-    grunt.registerTask('test', ['jshint', 'qunit']);
-  
-    grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-  
-  };
+    },
+    log: {
+      foo: [1, 2, 3],
+      bar: "Welcome to tutorialspoint",
+      sap: true,
+    },
+  });
+  grunt.registerTask("check", "check code", function () {
+    "node --check src/*.js";
+  });
+  grunt.registerMultiTask("log", "Log stuff.", function () {
+    grunt.log.writeln(this.target + ": " + this.data);
+  });
+};
