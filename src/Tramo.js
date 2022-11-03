38ddaaf61ed2b8383c9e67d6172684aa0fa298c8
JaimeGM96 <jaimegallardo@correo.ugr.es>
2022-11-03 13:41:50 +0100

    feat(Tramo.js): Redefinicion de la clase Tramo eliminando atributos innecesarios (#20)

--- src/Tramo.js
+++ src/Tramo.js
@@ -1,23 +1,17 @@
 /**
- * Declaración de la clase Tramo. Entidad. Esta clase representa un tramo de una ruta, que está compuesto por un punto de origen,
- * un destino, la distancia que hay entre esos dos puntos, el tiempo medio que se tarda en recorrer ese tramo
- * y por último la hora de inicio que influirá en la toma de decisiones debido a que el tráfico puede variar en función de la hora.
+ * Declaración de la clase Tramo. Objeto valor. Esta clase almacena los datos referentes a un tramo. Estos datos
+ * son punto de origen, punto de destino. Dos Tramos que tiene un mismo punto de origen y destino o 
+ * ,si el punto de origen es igual al punto de destino de otro y viceversa, son el mismo Tramo.
  */
 class Tramo {
     /**
      * Constructor de la clase Tramo
      * @param {String} origen Origen del tramo
      * @param {String} destino Destino del tramo
-     * @param {Number} distancia Distancia del tramo
-     * @param {Number} tiempoMedio Tiempo medio del tramo
-     * @param {Number} horaInicio Hora de inicio del tramo
      */
-    constructor (origen, destino, distancia, tiempoMedio, horaInicio) {
+    constructor (origen, destino) {
         this.origen = origen;
         this.destino = destino;
-        this.distancia = distancia;
-        this.tiempoMedio = tiempoMedio;
-        this.horaInicio = horaInicio;
     }
 
     /**
@@ -34,29 +28,5 @@ class Tramo {
      */
     get destino() {
         return this.destino;
-    }
-
-    /**
-     * Método que devuelve la distancia del tramo
-     * @returns {Number} distancia Distancia del tramo
-     */
-    get distancia() {
-        return this.distancia;
-    }
-
-    /**
-     * Método que devuelve el tiempo medio del tramo
-     * @returns {Number} tiempoMedio Tiempo medio del tramo
-     */
-    get tiempoMedio() {
-        return this.tiempoMedio;
-    }
-
-    /**
-     * Método que devuelve la hora de inicio del tramo
-     * @returns {Number} horaInicio Hora de inicio del tramo
-     */
-    get horaInicio() {
-        return this.horaInicio;
     }
 }
