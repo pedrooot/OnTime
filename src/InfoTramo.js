1ee5607eb2d538d2d02201eaae5233937e497ef0
Pedro Martín González <pedromarting3@gmail.com>
2022-11-10 10:50:49 +0100

    Avanza #25 creando InfoTramo

---
+++ src/InfoTramo.js
@@ -0,0 +1,13 @@
+//Creacion de la clase InfoTramo en la que se almacena información sobre las horas 
+//congestionadas en cada tramo
+
+const tramos = [{
+    origen: 'a',
+    destino: 'b',
+    hora: '9:20'
+},
+{
+    origen: 'c',
+    destino: 'd',
+    hora: '15:00'
+}];
