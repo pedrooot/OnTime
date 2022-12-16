f5942d971bb9396fcc4d3a7043c2861ddd3f86c3
Pedro Martín González <pedromarting3@gmail.com>
2022-12-16 09:33:46 +0100

    avanza #38 modificando gruntfile.js y añadiendo dockerfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,23 @@ module.exports = function (grunt)
   grunt.loadNpmTasks('grunt-exec');
 
   grunt.initConfig({
+    connect: {
+      server: {
+          options: {
+              base: "",
+              port: 8080
+          }
+      }
+    },
+    watch: {},
     exec:{
       jest: 'jest test/*.js'
     }
   })
 
+  grunt.loadNpmTasks( "grunt-contrib-connect" );
+  grunt.loadNpmTasks( "grunt-contrib-watch" );
+  grunt.registerTask( "dev", [ "connect", "watch" ] );
   grunt.registerTask('test','execute test', ['exec']);
   grunt.registerTask("check", "check code", function () {
     "node --check src/*.js";
